99e02670245fa5ad4882752d7ebb09659faaca7b
Dominik Hura <dominik125@gmail.com>
2021-04-19 15:23:33 +0200

    edited diod_prescaler & divide_count_ch4

--- diod_prescaler.vhd
+++ diod_prescaler.vhd
@@ -6,7 +6,6 @@ use IEEE.numeric_std.ALL;
 entity diod_prescaler is
 	generic(
 		INPUT_FREQUENCY : integer := 100000000;	 -- value input frequency
-		PRESCALER_DIVIDE : integer := 100000;  -- value to divide base input clock
 		DUTY_CYCLE : integer := 2;  -- duty cycle on %
 		NUMBER_OF_LED : integer := 6 
 		);
@@ -30,18 +29,24 @@ begin
 	
 	-- SET PROPER PRESCALER VALUE --
 	process(clk_in)
-	variable divide_count : integer range 0 to PRESCALER_DIVIDE :=0;
+	
+	--we/1000 = prescaler_divide == 1000 HZ
+	variable divide_count : integer range 0 to (INPUT_FREQUENCY/10000) :=0;
 	begin
 		if rising_edge(clk_in) then
-			if rst = '1' then
+			if rst = '0' then
 				divide_count := 0;
 				clk_div <= '0';
 			else		
-				if divide_count = PRESCALER_DIVIDE/2 then
-					clk_div <= not clk_div;
-					divide_count := 0;
+				if divide_count < (INPUT_FREQUENCY/10000)/2 then
+					clk_div <= '0';
 				else
-					divide_count := divide_count + 1;
+				    clk_div <= '1';
+				end if;
+				if divide_count = (INPUT_FREQUENCY/10000) then
+				    divide_count := 0;
+				else
+				    divide_count := divide_count + 1;
 				end if;
 			end if;
 		end if;
@@ -49,13 +54,12 @@ begin
 	
 	process(clk_in, clk_div, led_in_r)
 	
-	variable presc_count : integer range 0 to (INPUT_FREQUENCY / PRESCALER_DIVIDE) := 0;
-	constant presc_value : integer := (INPUT_FREQUENCY / PRESCALER_DIVIDE);
+	variable presc_count : integer range 0 to 100 := 0;
 	
 	begin
 		if rising_edge(clk_div) then
 			for i in 0 to NUMBER_OF_LED-1 loop
-				if presc_count < (DUTY_CYCLE*presc_value/100) and led_in_r(i) = '1' then
+				if presc_count < (DUTY_CYCLE) and led_in_r(i) = '1' then
 					led_out(i) <= '1';
 				else
 					led_out(i) <= '0';
--- divide_count_ch4.vhd
+++ divide_count_ch4.vhd
@@ -30,23 +30,27 @@ begin
 	-- CLOCK DIVIDE CHANNEL 0 --
 	process(clk_in_ch0)
 	variable count : integer range 0 to DIVIDE_VALUE_CH0 := 0;
-	
+	constant divide_ch0 : integer := DIVIDE_VALUE_CH0; 
 	begin
 		if rising_edge(clk_in_ch0) then
-			if sys_rst = '1' then
+			if sys_rst = '0' then
 				count := 0;
 				clk_out_ch0 <= '0';
 			else
-				if count < DIVIDE_VALUE_CH0/2 then
-					clk_out_ch0 <= '0';
-				else
-					clk_out_ch0 <= '1';
-				end if;
-				if count = DIVIDE_VALUE_CH0 then
-					count := 0;
-				else
-					count := count + 1;
-				end if;
+			    if(divide_ch0 = 1 or divide_ch0 = 0) then
+			        clk_out_ch0 <= clk_in_ch0;
+			    else
+                    if count < DIVIDE_VALUE_CH0/2 then
+                        clk_out_ch0 <= '0';
+                    else
+                        clk_out_ch0 <= '1';
+                    end if;
+                    if count = DIVIDE_VALUE_CH0 then
+                        count := 0;
+                    else
+                        count := count + 1;
+                    end if;
+                 end if;
 			end if;
 		end if;
 	end process;
@@ -54,23 +58,28 @@ begin
 	-- CLOCK DIVIDE CHANNEL 1 --
 	process(clk_in_ch1)
 	variable count : integer range 0 to DIVIDE_VALUE_CH1 :=0;
+	constant divide_ch1 : integer := DIVIDE_VALUE_CH1;
 	
 	begin
 		if rising_edge(clk_in_ch1) then
-			if sys_rst = '1' then
+			if sys_rst = '0' then
 				count :=0;
 				clk_out_ch1 <= '0';
 			else
-				if count < DIVIDE_VALUE_CH1/2 then
-					clk_out_ch1 <= '0';
-				else
-					clk_out_ch1 <= '1';
-				end if;
-				if count = DIVIDE_VALUE_CH1 then
-					count := 0;
-				else
-					count := count + 1;
-				end if;
+			    if(divide_ch1 = 1 or divide_ch1 = 0) then
+			        clk_out_ch1 <= clk_in_ch1;
+			    else
+                    if count < DIVIDE_VALUE_CH1/2 then
+                        clk_out_ch1 <= '0';
+                    else
+                        clk_out_ch1 <= '1';
+                    end if;
+                    if count = DIVIDE_VALUE_CH1 then
+                        count := 0;
+                    else
+                        count := count + 1;
+                    end if;
+                end if;   
 			end if;
 		end if;
 	end process; 
@@ -78,23 +87,28 @@ begin
 	-- CLOCK DIVIDE CHANNEL 2 --	
 	process(clk_in_ch2)
 	variable count : integer range 0 to DIVIDE_VALUE_CH2 :=0;
+	constant divide_ch2 : integer := DIVIDE_VALUE_CH2;
 	
 	begin
 		if rising_edge(clk_in_ch2) then
-			if sys_rst = '1' then
+			if sys_rst = '0' then
 				count :=0;
 				clk_out_ch2 <= '0';
 			else
-				if count < DIVIDE_VALUE_CH2/2 then
-					clk_out_ch2 <= '0';
-				else
-					clk_out_ch2 <= '1';
-				end if;
-				if count = DIVIDE_VALUE_CH2 then
-					count := 0;
-				else
-					count := count + 1;
-				end if;
+			    if(divide_ch2 = 1 or divide_ch2 = 0) then
+			        clk_out_ch2 <= clk_in_ch2;
+			    else
+                    if count < DIVIDE_VALUE_CH2/2 then
+                        clk_out_ch2 <= '0';
+                    else
+                        clk_out_ch2 <= '1';
+                    end if;
+                    if count = DIVIDE_VALUE_CH2 then
+                        count := 0;
+                    else
+                        count := count + 1;
+                    end if;
+                end if;
 			end if;
 		end if;
 	end process;
@@ -102,23 +116,28 @@ begin
 	-- CLOCK DIVIDE CHANNEL 3 --	
 	process(clk_in_ch3)
 	variable count : integer range 0 to DIVIDE_VALUE_CH3 :=0;
+	constant divide_ch3 : integer := DIVIDE_VALUE_CH3;
 	
 	begin
 		if rising_edge(clk_in_ch3) then
-			if sys_rst = '1' then
+			if sys_rst = '0' then
 				count :=0;
 				clk_out_ch3 <= '0';
 			else
-				if count < DIVIDE_VALUE_CH3/2 then
-					clk_out_ch3 <= '0';
-				else
-					clk_out_ch3 <= '1';
-				end if;
-				if count = DIVIDE_VALUE_CH3 then
-					count := 0;
-				else
-					count := count + 1;
-				end if;
+			    if(divide_ch3 = 1 or divide_ch3 = 0) then
+			        clk_out_ch3 <= clk_in_ch3;
+			    else
+                    if count < DIVIDE_VALUE_CH3/2 then
+                        clk_out_ch3 <= '0';
+                    else
+                        clk_out_ch3 <= '1';
+                    end if;
+                    if count = DIVIDE_VALUE_CH3 then
+                        count := 0;
+                    else
+                        count := count + 1;
+                    end if;
+                end if;
 			end if;
 		end if;
 	end process;
